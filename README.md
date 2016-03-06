61fc61bb6092439edcc56b3552d6169446a906c7
Henrik Boelsmand <wapbamboogie@hotmail.com>
2016-03-06 20:46:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bootstrap 4 boilerplate
+This repository contains the project code for my Bootstrap 4 boilerplate.
+A Bootstrap 4 boiler plate with sass, concatenation, minification, autoprefixer, browser-sync, git vcs and gulp task runner
+
+## Using this Repo
+This is a node project.
+
+## Quick Start
+```
+# Clone this repo
+git clone https://github.com/wapbamboogie/bootstrap-4-boilerplate.git
+# Navigate into the directory
+cd bootstrap-4-boilerplate
+# Install the project's node dependencies
+npm install
+# Gulp commands
+gulp - minify css/js and builds your app into dist directory
+gulp serve - compiles and watch sass, start browser-sync, autoprefixer, concenates js
+```
+
+## Installing gulp
+This course requires you to have global installations of [gulp](http://gulpjs.com/).
+```
+# Install gulp globally
+npm install -g gulp
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+     del = require('del'),
+     autoprefixer = require('gulp-autoprefixer'),
+     browserSync = require('browser-sync').create(),
+     htmlreplace = require('gulp-html-replace'),
+     cssmin = require('gulp-cssmin');
+
+gulp.task("concatScripts", function() {
+    return gulp.src([
+        'assets/js/jquery-2.2.1.min.js',
+        'assets/js/sticky/bootstrap.min.js',
+        'assets/js/functions.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('main.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('assets/js'));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("assets/js/main.js")
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('dist/assets/js'));
+});
+
+gulp.task('compileSass', function() {
+  return gulp.src("assets/css/main.scss")
+      .pipe(maps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('assets/css'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task("minifyCss", ["compileSass"], function() {
+  return gulp.src("assets/css/main.css")
+    .pipe(cssmin())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+gulp.task('watchFiles', function() {
+  gulp.watch('assets/css/**/*.scss', ['compileSass']);
+  gulp.watch('assets/js/main.js', ['concatScripts']);
+})
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('clean', function() {
+  del(['dist', 'assets/css/main.css*', 'assets/js/main*.js*']);
+});
+
+gulp.task('renameSources', function() {
+  return gulp.src('index.html')
+    .pipe(htmlreplace({
+        'js': 'assets/js/main.min.js',
+        'css': 'assets/css/main.min.css'
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task("build", ['minifyScripts', 'minifyCss'], function() {
+  return gulp.src(['index.html',
+                   "assets/img/**", "assets/fonts/**"], { base: './'})
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['watchFiles'], function(){
+  browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("assets/css/**/*.scss", ['watchFiles']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task("default", ["clean", 'build'], function() {
+  gulp.start('renameSources');
+});
